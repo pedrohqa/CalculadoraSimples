b480ade5404269d1721ce23be790bd9be497743c
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-05-04 09:33:47 -0300

    Commit do Pedro

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -12,19 +12,77 @@ public class CalculadoraSimples{
             System.out.println("\n\t\t\t--CalcuLdora Simples--\n");
             System.out.println("1. Soma");
             System.out.println("2. Subtração");
-            System.out.println("3. Sair");
+            System.out.println("3. Multiplicação");
+            System.out.println("4. Divisão");
+            System.out.println("5. Sair");
             System.out.print("Opção: ");
             op = entrada.nextInt();
 
             if(op == 1){
                 System.out.println("\n\t\t\tSoma\n");
-            } else if(op == 2){
+
+                //Entrada
+                System.out.println("Informe: N1 ");
+                n1 = entrada.nextInt();
+                System.out.println("Informe: N2 ");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1 + n2;
+
+                //Saida
+                System.out.printf("%d + %d = %d", n1, n2, total);
+
+            }else if(op == 2){
                 System.out.println("\n\t\t\tSubtração\n");
-            } else if(op == 3){
-                System.out.println("\n\t\t\tForte Abraço!\n");
-            } else{
+
+                //Entrada
+                System.out.println("Informe N1:");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2:");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1 - n2;
+
+                //saida
+                System.out.printf("%d - %d = %d", n1, n2, total);
+
+            }else if(op == 3){
+                System.out.println("\n\t\t\tMultiplicação\n");
+
+                //Entradas
+                System.out.println("Informe N1:");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2:");
+                n2 = entrada.nextInt();
+
+                //processamento
+                total = n1 * n2;
+
+                //saida
+                System.out.printf("%d * %d = %d", n1, n2, total);
+
+            }else if(op == 4){
+                System.out.println("\n\t\t\tDivisão\n");
+
+                //Entradas
+                System.out.println("informe N1:");
+                n1 = entrada.nextInt();
+                System.out.println("informe N2:");
+                n2 = entrada.nextInt();
+
+                //Processamento 
+                total = n1 / n2;
+
+                //Saida
+                System.out.printf("%d / %d = %d", n1, n2, total);
+
+            }else if(op == 5){
+                System.out.println("\n\t\t\tTchau!!\n");
+            }else{
                 System.out.println("Opção" +op + "incorreta");
             }
-        }while(op!=3);
+        }while(op!=5);
     }
 }
